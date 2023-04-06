d6eb0df0cf53a3d3454272b75de04f615219d723
IrshakAnna-Mariia <irshakam@gmail.com>
2023-04-06 13:13:42 +0300

    feat: cqs landing section

--- src/pages/Landing/RoadMap/RoadMap.tsx
+++ src/pages/Landing/RoadMap/RoadMap.tsx
@@ -1,13 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 import Button from "components/Button";
 import SectionHeader from "components/SectionHeader";
+import { PathName } from "enums/pathNames";
 import { ReactComponent as RoadMapImg } from "icons/roadMap.svg";
 
 const RoadMap = () => {
+  const navigate = useNavigate();
+
+  const handleViewProgress = () => {
+    navigate(PathName.Profile);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center mt-24">
       <SectionHeader title={'ROAD MAP'} />
       <RoadMapImg className="w-3/5" />
-      <Button label="View your progress" onClick={() => {}} styleForm="pill" size="lg" />
+      <Button label="View your progress" onClick={handleViewProgress} styleForm="pill" size="lg" />
     </div>
   );
 };
