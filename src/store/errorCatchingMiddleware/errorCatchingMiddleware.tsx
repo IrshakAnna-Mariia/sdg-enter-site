ccdd40b462c5e1ae250383d2b5bfed717db27ef1
IrshakAnna-Mariia <irshakam@gmail.com>
2023-06-12 00:33:11 +0300

    feat: error handling for requests

---
+++ src/store/errorCatchingMiddleware/errorCatchingMiddleware.tsx
@@ -0,0 +1,13 @@
+import { isRejectedWithValue } from '@reduxjs/toolkit';
+import type { Middleware } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+
+export const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
+  if (isRejectedWithValue(action)) {
+    const errorMessage = action.payload.data?.message || action.payload.message || 'Something went wrong...';
+    toast.error(errorMessage);
+  }
+
+  return next(action);
+};
