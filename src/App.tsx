65242d09e1c3a49d5cc5a888765a6a6525ca24a4
IrshakAnna-Mariia <irshakam@gmail.com>
2023-06-12 00:35:39 +0300

    feat: notification to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import 'index.css';
 
 function App() {
   return (
-    <Outlet />
+    <>
+      <ToastContainer position="top-center" theme="light" style={{ width: '45%' }} />
+      <Outlet />
+    </>
   );
 }
 
