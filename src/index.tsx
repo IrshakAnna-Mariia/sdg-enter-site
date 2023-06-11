0dd04adc8b0507a02e7079d55e02ae614d6bd002
IrshakAnna-Mariia <irshakam@gmail.com>
2023-06-12 00:36:05 +0300

    feat: store provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import ScrollToTop from 'react-scroll-to-top';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { ReactComponent as ScrollTop } from 'icons/scrollTop.svg';
 import { routerConfig } from 'router';
 
+import { persistor, store } from './store';
 import reportWebVitals from './reportWebVitals';
+import Loader from 'components/Loader';
 
 const router = createBrowserRouter(createRoutesFromElements(routerConfig));
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    {/* <PersistGate persistor={persistor} loading={<Loader isVisible />}> */}
+    <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<Loader isVisible />}>
     <RouterProvider router={router} />
     <ScrollToTop
       smooth
       style={{ backgroundColor: 'transparent', borderRadius: '9999px' }}
       component={<ScrollTop className="h-12 w-12" />}
     />
-    {/* </PersistGate> */}
-    {/* </Provider> */}
+    </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
 
