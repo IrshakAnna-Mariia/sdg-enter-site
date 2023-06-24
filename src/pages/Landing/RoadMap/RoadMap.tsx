d0136e0cb2edf0bc1ce61450cca0737ae068b66c
IrshakAnna-Mariia <irshakam@gmail.com>
2023-06-24 20:49:27 +0300

    fix: road map view more button

--- src/pages/Landing/RoadMap/RoadMap.tsx
+++ src/pages/Landing/RoadMap/RoadMap.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'components/Button';
 import SectionHeader from 'components/SectionHeader';
+import { PathName } from 'enums/pathNames';
 import { ReactComponent as RoadMapImg } from 'icons/roadMap.svg';
 
-const RoadMap = () => {
+const RoadMap: React.FC<{ isLanding?: boolean }> = ({ isLanding }) => {
+  const navigate = useNavigate();
+
+  const handleViewRoadMap = () => {
+    navigate(PathName.RoadMap);
+  };
+
   return (
     <div className="mt-24 flex min-h-screen flex-col items-center">
       <SectionHeader title={'ROAD MAP'} />
       <RoadMapImg className="w-3/5" />
+      {isLanding && <Button label="View more" onClick={handleViewRoadMap} styleForm="pill" size="lg" />}
     </div>
   );
 };
